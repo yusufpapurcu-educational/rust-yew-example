f24bf4515f7b31b0ffeb035054be91d90b2919f7
Yusuf Papurcu <yusufturhanp@gmail.com>
2022-02-05 12:31:21 +0300

    init

---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use yew::prelude::*;
+
+struct Model{
+    value: i64
+}
+
+#[function_component(App)]
+fn app() -> Html{
+    let state = use_state(|| Model{
+        value: 0
+    });
+    
+    let onclick = {
+        let state = state.clone();
+
+        Callback::from(move |_|{
+            state.set(Model{
+                value: state.value + 1
+            })
+        })
+    };
+    
+    html!{
+        <div>
+        <button {onclick}>{"Click Me!"}</button>
+        <p>{ state.value }</p>
+        </div>
+    }
+}
+ 
+fn main(){
+    yew::start_app::<App>();
+}
